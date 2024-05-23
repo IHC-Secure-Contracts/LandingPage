aae0354c5baef18502a603febb788e288182d544
jhosepmyr <jhosepmyrgutierrezsoto@gmail.com>
2024-05-22 22:08:32 -0500

    chore: added DOM elements

---
+++ src/index.js
@@ -0,0 +1,7 @@
+const buttonMobile = document.querySelector('#display-button');
+const menuOpcions = document.querySelector('.navbar__complete-list');
+const navbar = document.querySelector('.navbar');
+const header = document.querySelector('.header');
+const cardsQuestions = document.querySelectorAll('.preguntas-frecuentes__card');
+const linkOpciones = document.querySelectorAll('.navbar__link');
+const buttonContactNavbar = document.querySelector('.navbar__button');
