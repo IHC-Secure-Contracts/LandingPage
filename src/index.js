33b9203e14d310827e5bdba8c6a329ac2ba69bc2
jhosepmyr <jhosepmyrgutierrezsoto@gmail.com>
2024-05-22 22:11:44 -0500

    feat:  showOpcions in navbar mobile

--- src/index.js
+++ src/index.js
@@ -5,3 +5,19 @@ const header = document.querySelector('.header');
 const cardsQuestions = document.querySelectorAll('.preguntas-frecuentes__card');
 const linkOpciones = document.querySelectorAll('.navbar__link');
 const buttonContactNavbar = document.querySelector('.navbar__button');
+
+
+
+
+
+function showOpcions(){
+    menuOpcions.classList.toggle('navbar__complete-list--show_list');
+    if(menuOpcions.classList.contains('navbar__complete-list--show_list')){
+        navbar.classList.add('navbar--dark-background');
+    } 
+    if(!(
+        menuOpcions.classList.contains('navbar__complete-list--show_list')) 
+        && document.documentElement.scrollTop < header.clientHeight){
+        navbar.classList.remove('navbar--dark-background');
+    }
+}
