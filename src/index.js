57db9109d64214afd32c84b74af2098a994ce068
jhosepmyr <jhosepmyrgutierrezsoto@gmail.com>
2024-06-06 03:21:47 -0500

    feat(testimonios): carrusel done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import { usersJSON } from "./routes.js";
+
 const buttonMobile = document.querySelector('#display-button');
 const menuOpcions = document.querySelector('.navbar__complete-list');
 const navbar = document.querySelector('.navbar');
@@ -6,13 +8,14 @@ const cardsQuestions = document.querySelectorAll('.preguntas-frecuentes__card');
 const linkOpciones = document.querySelectorAll('.navbar__link');
 const buttonContactNavbar = document.querySelector('.navbar__button');
 const testimoniosContent = document.querySelector('div.testimonios__content');
+const apiRandomUser = "https://randomuser.me/api/";
 
 let stepCarrusel = 1;
 let stop;
 let start;
 
 window.addEventListener('load', ()=>{
-    carruselAutomatic();
+    generateTestimonios();
 })
 buttonMobile.addEventListener("click", showOpcions);
 window.addEventListener("resize", restarOpcion);
@@ -71,10 +74,44 @@ function carruselAutomatic(){
             testimoniosContent.scrollLeft = testimoniosContent.scrollLeft + stepCarrusel;
             if(testimoniosContent.scrollLeft === maxScrollLeft) stepCarrusel = stepCarrusel * -1;
             if(testimoniosContent.scrollLeft === 0) stepCarrusel = stepCarrusel * -1;
-        }, 50);
+            console.log(stepCarrusel, testimoniosContent.scrollLeft, maxScrollLeft);
+        }, 15);
     }
     stop = () =>{
         clearInterval(interval);
     };
     start();
+}
+function builtTestimonio(srcImg, commentUser, nameUser, occupationUser){
+    const testimonio = document.createElement('article');
+    testimonio.className = 'testimonios__card';
+    testimonio.innerHTML = `
+        <img class="testimonios__person-photo" src="${srcImg}" alt="Foto ilustrativa de un beneficiario">
+        <h6 class="testimonios__comment">"${commentUser}"</h6>
+        <p class="testimonios__person">
+            <span>${nameUser}</span>
+            <span>${occupationUser}</span>
+        </p>
+    `
+    return testimonio;
+}
+async function getDataRandomUser(cantidad) {
+    const response = await fetch(`${apiRandomUser}?results=${cantidad}&nat=us`);
+    const data = await response.json();
+    return data.results;
+}
+async function generateTestimonios(){
+    const amountTest = usersJSON.comentarios.length;
+    const usersRandom = await getDataRandomUser(amountTest);
+    const listTest = [];
+    await usersRandom.forEach((user, index) => {
+        const srcImg = user.picture.medium;
+        const commentUser = usersJSON.comentarios[index];
+        const nameUser = `${user.name.first} ${user.name.last}`;
+        const occupationUser = usersJSON.oficios[index];
+        const testimonio = builtTestimonio(srcImg, commentUser, nameUser, occupationUser);
+        listTest.push(testimonio);
+    })
+    testimoniosContent.append(...listTest);
+    await carruselAutomatic();
 }
